486c83772dffaed8f1efca3217062d309216f6b9
Furkan Çentek <56794915+rootcf@users.noreply.github.com>
2021-12-18 03:59:15 +0300

    Update api.php

--- api.php
+++ api.php
@@ -1,37 +1,38 @@
 <?php
+class IPlayerService
+{
 
-class IPlayerService{
-
-  
-  //GetSteamLevel
-  // For more information, visit https://partner.steamgames.com/doc/webapi/IPlayerService#GetSteamLevel
-    function GetSteamLevel($apikey,$userid){
-        $api_url = "https://api.steampowered.com/IPlayerService/GetSteamLevel/v1/?key=".$apikey."&steamid=".$userid;
-        $json = json_decode(file_get_contents($api_url),true);
+    //GetSteamLevel
+    // For more information, visit https://partner.steamgames.com/doc/webapi/IPlayerService#GetSteamLevel
+    function GetSteamLevel($apikey, $userid)
+    {
+        $api_url = "https://api.steampowered.com/IPlayerService/GetSteamLevel/v1/?key=" . $apikey . "&steamid=" . $userid;
+        $json = json_decode(file_get_contents($api_url) , true);
         return $json['response'];
     }
 
-
 }
 
-class ISteamUser{
+class ISteamUser
+{
 
-//GetPlayerSummaries
-// For more information, visit https://partner.steamgames.com/doc/webapi/ISteamUser#GetPlayerSummaries
-function GetPlayerSummaries($apikey,$userid,$param){
-$api_url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=".$apikey."&steamids=".$userid;
-$json = json_decode(file_get_contents($api_url),true);
-switch($param){
-case "getall":
-return var_dump($json['response']['players'][0]);
-break;
-default;
-return $json['response']['players'][0][$param];
-   }
- }
-
-
+    //GetPlayerSummaries
+    // For more information, visit https://partner.steamgames.com/doc/webapi/ISteamUser#GetPlayerSummaries
+    function GetPlayerSummaries($apikey, $userid, $param)
+    {
+        $api_url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" . $apikey . "&steamids=" . $userid;
+        $json = json_decode(file_get_contents($api_url) , true);
+        switch ($param)
+        {
+            case "getall":
+                return var_dump($json['response']['players'][0]);
+            break;
+            default;
+            return $json['response']['players'][0][$param];
+    }
 }
 
+}
 
 ?>
+
