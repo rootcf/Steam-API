3fa9a0da2498a2f22f4314e31cabe84c77d4c8b0
Furkan Çentek <56794915+rootcf@users.noreply.github.com>
2021-12-18 03:57:41 +0300

    Update api.php

--- api.php
+++ api.php
@@ -1,5 +1,23 @@
 <?php
-//Get Player Info
+
+class IPlayerService{
+
+  
+  //GetSteamLevel
+  // For more information, visit https://partner.steamgames.com/doc/webapi/IPlayerService#GetSteamLevel
+    function GetSteamLevel($apikey,$userid){
+        $api_url = "https://api.steampowered.com/IPlayerService/GetSteamLevel/v1/?key=".$apikey."&steamid=".$userid;
+        $json = json_decode(file_get_contents($api_url),true);
+        return $json['response'];
+    }
+
+
+}
+
+class ISteamUser{
+
+//GetPlayerSummaries
+// For more information, visit https://partner.steamgames.com/doc/webapi/ISteamUser#GetPlayerSummaries
 function GetPlayerSummaries($apikey,$userid,$param){
 $api_url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=".$apikey."&steamids=".$userid;
 $json = json_decode(file_get_contents($api_url),true);
@@ -9,9 +27,11 @@ return var_dump($json['response']['players'][0]);
 break;
 default;
 return $json['response']['players'][0][$param];
-}
-}
+   }
+ }
 
+
+}
 
 
 ?>
